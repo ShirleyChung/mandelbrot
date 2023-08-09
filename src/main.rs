ccbdce4e6776a758fd3c6fbaca5957625b14c63a
鐘妘甄 <shirley@mail.kway.com>
2023-08-09 17:27:13 +0800

    init commit

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use num::Complex;
+use std::str::FromStr;
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None
+            }
+        }
+    }
+}
+
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex {re, im}),
+        None => None
+    }
+}
+
+fn complex_square_add_loop(c: Complex<f64>) {
+    let mut z = Complex {re: 0.0, im: 0.0};
+    loop {
+        z = z + c;
+    }
+}
+
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex {re: 0.0, im: 0.0};
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z + c;
+    }
+    None
+}
+
+fn main() {
+    println!("Hello, world!");
+}
