3227ada6eff7730fb8fd7bdd0dc3aafa5a5a26ed
ShirleyChung <jonesr_tw@yahoo.com.tw>
2023-08-10 16:35:50 +0800

    fix mandelbrot rendering

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn parse_complex(s: &str) -> Option<Complex<f64>> {
 fn complex_square_add_loop(c: Complex<f64>) {
     let mut z = Complex {re: 0.0, im: 0.0};
     loop {
-        z = z + c;
+        z = z * z + c;
     }
 }
 
@@ -37,7 +37,7 @@ fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
         if z.norm_sqr() > 4.0 {
             return Some(i);
         }
-        z = z + c;
+        z = z * z + c;
     }
     None
 }
